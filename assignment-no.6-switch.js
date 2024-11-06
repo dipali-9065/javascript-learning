8a428b479b630247847fa2c28f16141aedc98b5e
dipali-9065 <dipaliyerekar009@gmail.com>
2024-11-06 09:04:46 +0530

    Assignment-no.6-switch
    switch case

---
+++ assignment-no.6-switch.js
@@ -0,0 +1,57 @@
+function month(MonthNo){
+    switch (MonthNo) {
+     case 0:
+         console.log("January");    
+         break;
+     case 1:
+         console.log("February");    
+         break;
+     case 2:
+         console.log("march");    
+         break;
+     case 3:
+         console.log("April");    
+         break;
+     case 4:
+         console.log("May");    
+         break;
+     case 5:
+         console.log("June");    
+         break;
+     case 6:
+         console.log("July");    
+         break;
+    case 7:
+         console.log("August");    
+         break;
+    case 8:
+         console.log("September ");    
+         break;
+    case 9:
+         console.log("October");    
+         break;
+    case 10:
+         console.log("November");    
+         break;
+    case 11:
+         console.log("December");    
+         break;
+   
+     default:
+         console.log("Invalid month number: ", MonthNo);
+         break;
+    }
+ }
+ month(0);
+ month(1);
+ month(2);
+ month(3);
+ month(4);
+ month(5);
+ month(6);
+ month(7);
+ month(8);
+ month(9);
+ month(10);
+ month(11);
+ month(20);
